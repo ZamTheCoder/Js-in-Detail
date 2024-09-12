d906004ab276c6c1ccdcfeb8f8226a15bee9da33
ZamTheCoder <rudhav935@gmail.com>
2024-09-12 20:47:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+add = (a, b) => a + b
+
+sub = (c, d) => c - d
+
+mul = (e, f) => e * f
+
+div = (g, h) => g / h
+
+document.write(add(2, 4) + "<br>")
+
+document.write(sub(4, 2) + "<br>")
+
+document.write(mul(2, 4) + "<br>")
+
+document.write(div(2, 4) + "<br>")
+
